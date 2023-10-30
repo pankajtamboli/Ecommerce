bd76404fcd7077792c4176b690fed6ba4c207607
pankajtamboli <pankajtamboli96@gmail.com>
2023-10-31 00:00:17 +0530

    ecommerse

---
+++ react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/component/navigation/Navigation';
+
+function App() {
+  return (
+    <div className="">
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
